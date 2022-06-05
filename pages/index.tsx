e2e91ddea1435a691756aa0815b267047adddafa
Jack Allard <jackallard17@gmail.com>
2022-06-05 16:57:10 -0700

    temporarily removing bootstrap import

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { FormControl, FormGroup, Form, Button } from 'react-bootstrap'
+// import { FormControl, FormGroup, Form, Button } from 'react-bootstrap'
 
 const Home: NextPage = () => {
   return (
