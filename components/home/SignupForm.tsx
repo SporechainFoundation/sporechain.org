bae88abb8ada0e29e7c813f0059b8bbffe2896da
Jack Allard <jackallard17@gmail.com>
2022-06-08 15:07:08 -0700

    fixed form props

--- components/home/SignupForm.tsx
+++ components/home/SignupForm.tsx
@@ -2,12 +2,7 @@ import React, { Component, useState } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-interface SignupFormProps {
-  email: string,
-  name: string
-}
-
-function SignupForm(props: SignupFormProps) {
+function SignupForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
@@ -32,10 +27,10 @@ function SignupForm(props: SignupFormProps) {
     <Form className="p-4 p-md-5 border rounded-3 bg-light" onSubmit={handleSubmit}>
       <h2>Join the Mailing List</h2>
       <Form.Group className="mb-3">
-        <Form.Control required value={props.email} type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <Form.Control required type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3">
-        <Form.Control value={props.name} type="text" placeholder="Name (optional)" onChange={(e) => setName(e.target.value)} />
+        <Form.Control type="text" placeholder="Name (optional)" onChange={(e) => setName(e.target.value)} />
       </Form.Group>
       <Button type="submit" className="w-100 btn btn-lg btn-primary">Sign Up</Button>
     </Form>
