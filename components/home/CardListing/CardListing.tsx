ed03c8d49d78a9aca7c30279e40ca6abc51bbbd9
Jack Allard <jackallard17@gmail.com>
2022-06-15 23:01:58 -0700

    added responsive layout to CardListing

--- components/home/CardListing/CardListing.tsx
+++ components/home/CardListing/CardListing.tsx
@@ -5,37 +5,25 @@ import { Card } from "react-bootstrap";
 function CardListing() {
   return (
     <>
-
-      {/* <div className="container-fluid">
-      <div className={`${styles.flexContainer} ${styles.main}`}>
-
-        <div className="col-md-10 text-center mx-auto col-lg-5">
-          <h2 className={styles.icon}>🕮 </h2>
-            <h2>Open Source</h2>
-        </div>
-        <div className="col-md-10 text-center mx-auto col-lg-5">
-            <h2 className={styles.icon}>🌐</h2>
-          <h2 >Unified</h2>
-        </div>
-        <div className="col-md-10 text-center mx-auto col-lg-5">
-            <h2 className={styles.icon}>🖧</h2>
-          <h2>Decentralized </h2>
-        </div>
-      </div>
-      </div> */}
       <div className={`${styles.flexContainer} ${styles.main}`}>
         <div className="row align-items-center g-lg-5 py-5">
           <div className={`${styles.flexItem} text-center`}>
             <h2 className={styles.icon}>🕮 </h2>
             <h2>Open Source</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
           <div className={`${styles.flexItem} text-center`}>
             <h2 className={styles.icon}>🌐</h2>
             <h2 >Unified</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
           <div className={`${styles.flexItem} text-center`}>
             <h2 className={styles.icon}>🖧</h2>
             <h2>Decentralized </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
           <div className="col-md-10 mx-auto col-lg-5">
           </div>
