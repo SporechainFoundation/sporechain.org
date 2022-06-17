414c115b6a61cc4f5782ad95cf5357c3bb822f51
Jack Allard <jackallard17@gmail.com>
2022-06-16 21:27:45 -0700

    added content to card listings

--- components/home/CardListing/CardListing.tsx
+++ components/home/CardListing/CardListing.tsx
@@ -11,23 +11,21 @@ function CardListing() {
           <div className={`${styles.flexItem} text-center`}>
             <img className={styles.icon} src="/icons/book.svg" />
             <h2>Open Source</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+              {/* <p>Built on an open-source philosophy, the Sporechain couples decentralized file sharing with a dedicated blockchain to create a framework that synchronizes the efforts of a collaborative global community of independent contributors, each working in concert to collect, catalog, analyze, preserve, breed, propagate and distribute the genetics upon which we all rely.</p> */}
           </div>
           <div className={`${styles.flexItem} text-center`}>
             <img className={styles.icon} src="/icons/globe-svgrepo-com.svg" />
             <h2 >Unified</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>*/}
           </div>
           <div className={`${styles.flexItem} text-center`}>
             {/* <h2 className={styles.icon}>🖧</h2> */}
             <img className={styles.icon} src="/icons/network-svgrepo-com.svg" />
             <h2>Decentralized </h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. */}
           </div>
           <div className={`${styles.flexContainer} ${styles.main}`}>
+        <h3 className={`${styles.para} `}>Built on an open-source philosophy, the Sporechain couples decentralized file sharing with a dedicated blockchain to create a framework that synchronizes the efforts of a collaborative global community of independent contributors, each working in concert to collect, catalog, analyze, preserve, breed, propagate and distribute the genetics upon which we all rely.</h3>
             <div className={`${styles.flexItem} text-center`}>
               <a href='#section3'>
                 <svg className={homeStyles.arrows}>
--- components/home/GetInvolved/GetInvolved.tsx
+++ components/home/GetInvolved/GetInvolved.tsx
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import styles from '../CardListing/CardListing.module.css'
-import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function GetInvolved() {
   return (
@@ -11,24 +11,27 @@ function GetInvolved() {
           <div className={`${styles.flexItem} text-center`}>
             <img className={styles.icon} src="/icons/programming-code-svgrepo-com.svg" /> 
             <h2>Development</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <h4>Engineer sustainable global solutions</h4>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
           </div>
           <div className={`${styles.flexItem} text-center`}>
             <img className={styles.icon} src="/icons/like-heart-svgrepo-com.svg" /> 
             <h2 >Community</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <h4>Spread the word and gather support</h4>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
           </div>
           <div className={`${styles.flexItem} text-center`}>
             <img className={styles.icon} src="/icons/law-svgrepo-com.svg" /> 
             <h2>Legal</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <h4>Protect the protectors</h4>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
           </div>
-          <div className="col-md-10 mx-auto col-lg-5">
-          </div>
+        <h4 className={`${styles.para}`}><span className={styles.big}>The Sporechain Foundation</span> is a 501(c)(3) non-profit dedicated to fostering the development, integration and promotion of the Sporechain. Reach out to us if you’d like to get in on the action, and sign up for the mailing list for updates about our first public information session and other upcoming events.</h4>
+      <div className={`${styles.flexContainer} ${styles.main}`}>
+        <a href=""><h2><u>Contact Us</u></h2></a>
+        <a href=""><h2><u>Donate</u></h2></a>
         </div>
+      </div>
       </div>
     </>
   )
